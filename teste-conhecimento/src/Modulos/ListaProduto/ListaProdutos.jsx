64b3d0f7c25c8445fc170e83f0be3ae3d12ec4a1
Pablo Redig <PabloRedigmrv@gmail.com>
2023-11-21 19:29:32 -0300

    Criado a função de Mostrar mais e Mostrar menos

--- teste-conhecimento/src/Modulos/ListaProduto/ListaProdutos.jsx
+++ teste-conhecimento/src/Modulos/ListaProduto/ListaProdutos.jsx
@@ -6,12 +6,12 @@ import imagem from "../../img/img_6.png";
 
 const ApiComponent = () => {
     const [data, setData] = useState([]);
+    const [showAll, setShowAll] = useState(false);
 
     useEffect(() => {
-
         const fetchData = async () => {
             try {
-                const response = await fetch('http://localhost:5000/products'); // Substitua pela URL da sua API
+                const response = await fetch('http://localhost:5000/products');
                 const jsonData = await response.json();
                 setData(jsonData);
             } catch (error) {
@@ -22,22 +22,34 @@ const ApiComponent = () => {
         fetchData();
     }, []);
 
-    return <>
-        <div className={style.Container}>
-            <div className={style.Produtos}>
-                {data.map(produto => (
-                    < div className={style.Card}>
-                        <img src={imagem} alt={`Imagem ${produto.name}`} />
-                        <label className={style.Card__Name} >{produto.name}</label>
-                        <label className={style.Card__Valor} >{`R$ ${produto.price}`}</label>
-                        <label className={style.Card__Parcela} >{produto.parcelamento}</label>
-                        <button className={style.btn}>COMPRAR</button>
-                    </div>
-                ))}
+    const handleLoadMore = () => {
+        setShowAll(!showAll);
+    };
+
+    const displayedData = showAll ? data : data.slice(0, 9);
+
+    return (
+        <>
+            <div className={style.Container}>
+                <div className={style.Produtos}>
+                    {displayedData.map((produto, index) => (
+                        <div key={index} className={style.Card}>
+                            <img src={imagem} alt={`Imagem ${produto.name}`} />
+                            <label className={style.Card__Name} >{produto.name}</label>
+                            <label className={style.Card__Valor} >{`R$ ${produto.price}`}</label>
+                            <label className={style.Card__Parcela} >{produto.parcelamento}</label>
+                            <button className={style.btn}>COMPRAR</button>
+                        </div>
+                    ))}
+                </div>
+
+                <button className={style.btn__CarregarMais} onClick={handleLoadMore}>
+                    {showAll ? 'MOSTRAR MENOS' : 'CARREGAR MAIS'}
+                </button>
             </div>
+        </>
+    );
+};
 
-            <button className={style.btn__CarregarMais}>CARREGAR MAIS</button>
-        </div>
-    </>
+export default ApiComponent;
 
-}; export default ApiComponent;
